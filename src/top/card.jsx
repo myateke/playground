eb66a674f0b34c3f2f0d956591543671ddf4ff59
myatake <m.yatake@gemcook.com>
2017-10-24 14:59:48 +0900

    Grid追加

--- src/top/card.jsx
+++ src/top/card.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { Card,
-         Flag
-       } from 'semantic-ui-react';
+import {
+  Grid,
+  Card,
+  Flag
+} from 'semantic-ui-react';
 
 import './../assets/stylesheets/gc-common.css';
 import './../assets/stylesheets/styles.css';
@@ -10,25 +12,35 @@ import './../assets/stylesheets/styles.css';
 import Logo_1 from './../assets/img/logo_3.jpg';
 
 const ProfileCard = () => (
-  <Card>
-    <img src={Logo_1} alt="" className="gc-width-100 height-auto" />
-    <Card.Content>
-      <Card.Header>
-        Masahiro
-      </Card.Header>
-      <Card.Meta>
-        <span className='date'>
-          Joined in July
-        </span>
-      </Card.Meta>
-      <Card.Description>
-        プロフィール
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <Flag name='france' />
-    </Card.Content>
-  </Card>
+    <Grid
+      container
+    >
+      <Grid.Column
+        mobile={16}
+        tablet={16}
+        computer={16}
+      >
+      <Card>
+        <img src={Logo_1} alt="" className="gc-width-100 height-auto" />
+        <Card.Content>
+          <Card.Header>
+            Masahiro
+          </Card.Header>
+          <Card.Meta>
+            <span className='date'>
+              Joined in July
+            </span>
+          </Card.Meta>
+          <Card.Description>
+            プロフィール
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Flag name='france' />
+        </Card.Content>
+      </Card>
+    </Grid.Column>
+  </Grid>
 );
 
 export default ProfileCard;
--- src/top/list.jsx
+++ src/top/list.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import { List } from 'semantic-ui-react';
+import {
+  Grid,
+  List
+} from 'semantic-ui-react';
 
 const ListDivided = () => (
-  <List divided relaxed className="pAll20">
-    <List.Item>
-      <List.Icon name='github' size='large' verticalAlign='middle' />
-      <List.Content>
-        <List.Header as='a' href="https://github.com/myateke">github</List.Header>
-        <List.Description>調査用リポジトリ</List.Description>
-      </List.Content>
-    </List.Item>
-    <List.Item>
-      <List.Icon name='write' size='large' verticalAlign='middle' />
-      <List.Content>
-        <List.Header as='a' href="http://qiita.com/gcyata">Qiita</List.Header>
-        <List.Description>勉強内容記事</List.Description>
-      </List.Content>
-    </List.Item>
-  </List>
+  <Grid
+    container
+  >
+    <Grid.Column
+      mobile={16}
+      tablet={16}
+      computer={10}
+    >
+      <List divided relaxed className="pAll20">
+        <List.Item>
+          <List.Icon name='github' size='large' verticalAlign='middle' />
+          <List.Content>
+            <List.Header as='a' href="https://github.com/myateke">github</List.Header>
+            <List.Description>調査用リポジトリ</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon name='write' size='large' verticalAlign='middle' />
+          <List.Content>
+            <List.Header as='a' href="http://qiita.com/gcyata">Qiita</List.Header>
+            <List.Description>勉強内容記事</List.Description>
+          </List.Content>
+        </List.Item>
+      </List>
+    </Grid.Column>
+  </Grid>
 );
 
 export default ListDivided;
